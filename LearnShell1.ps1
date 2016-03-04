11f8a18e447095686099aabdd44d1cd2c92743b0
mufana <mufana@live.nl>
2016-03-04 10:54:24 +0100

    Create LearnShell1.ps1

---
+++ LearnShell1.ps1
@@ -0,0 +1,29 @@
+# List directory contents
+
+Write-Host -ForegroundColor Cyan "List content with 'dir " 
+    dir C:\Temp
+Write-Host -ForegroundColor Cyan "List content with 'ls " 
+    ls C:\Temp
+Write-Host -ForegroundColor Cyan "List content with 'Get-ChildItem " 
+    Get-ChildItem C:\Temp
+
+# Get all aliases defined for <Get-ChildItem>
+    Get-Alias -Definition Get-ChildItem
+
+# Get all aliases for your local Windows Box
+    Get-Alias
+
+# Create new alias for <Select-String>
+New-Alias -Name grep -Value Select-String
+
+# Proof that the newly created alias exists
+    Get-Alias -Definition Select-String
+
+# Use your new alias
+    cat 'somefile' | grep 'sometext'
+
+# Show all commands on your local Windows Box
+    Get-Command
+
+# Show all commands on your local Windows Box
+    Get-Comand | Out-GridView
