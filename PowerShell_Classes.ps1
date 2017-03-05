ec54a5a8487c9307584a81b846fd6cb5bff12e4a
mufana <mufana@live.nl>
2017-03-05 01:24:12 +0100

    PowerShell Classes

---
+++ PowerShell_Classes.ps1
@@ -0,0 +1,13 @@
+﻿Class Car {
+
+    [string]$Model
+    [string]$Type
+    [int]$Miles
+    
+    [Void]Drive([int]$Miles){
+        $this.Miles += $Miles
+    }
+}
+
+$MyCar = [Car]::new()
+$MyCar.Model = "Massarati"$MyCar.Type = "Quatroporte"$Mycar.Miles = "1000"$MyCar.Drive(200)
